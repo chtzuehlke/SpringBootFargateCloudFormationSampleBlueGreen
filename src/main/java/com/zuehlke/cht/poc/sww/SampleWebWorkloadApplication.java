6b39ab7f148475dba831562cd01467ec0e6c3b42
Christian Tschenett <cht@zuehlke.com>
2019-07-16 15:20:10 +0200

    test pip

--- src/main/java/com/zuehlke/cht/poc/sww/SampleWebWorkloadApplication.java
+++ src/main/java/com/zuehlke/cht/poc/sww/SampleWebWorkloadApplication.java
@@ -35,7 +35,7 @@ public class SampleWebWorkloadApplication {
 			}
 		}
 		
-		sb.append("(v4.2) foo.bar=" + jdbcTemplate.queryForObject("select bar from foo", Integer.class));
+		sb.append("(v4.3) foo.bar=" + jdbcTemplate.queryForObject("select bar from foo", Integer.class));
 		
 		String response = sb.toString();
 		
