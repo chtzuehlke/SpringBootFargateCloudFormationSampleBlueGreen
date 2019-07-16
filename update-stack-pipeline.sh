410eebcefed2b0c40deb860953426f3ecc88bbf3
Christian Tschenett <cht@zuehlke.com>
2019-07-16 13:50:28 +0200

    pipeline test

--- update-stack-pipeline.sh
+++ update-stack-pipeline.sh
@@ -6,6 +6,13 @@ STACK_PREFIX="helloworld-$PREFIX"
 TARGET_PREFIX=${2:-default}
 TARGET_STACK_PREFIX="helloworld-$TARGET_PREFIX"
 
+TARGET_DB_PASSWORD_PARAM_NAME=$TARGET_STACK_PREFIX-db-pwd
+
+TARGET_TARGET_GROUP1=$(./get-stack-output.sh $TARGET_STACK_PREFIX-alb TargetGroup)
+TARGET_TARGET_GROUP2=$(./get-stack-output.sh $TARGET_STACK_PREFIX-alb TargetGroup2)
+
+TARGET_CLOUD_FORMATION_ROLE=$(./get-stack-output.sh $TARGET_STACK_PREFIX-sg CloudFormationRole)
+
 aws cloudformation update-stack --capabilities CAPABILITY_IAM --stack-name $STACK_PREFIX-pipe --template-body file://cloudformation/pipeline.yaml --parameters \
 	ParameterKey=CodeCommitRepositoryARN,UsePreviousValue=true \
 	ParameterKey=CodeCommitRepositoryName,UsePreviousValue=true \
@@ -20,4 +27,10 @@ aws cloudformation update-stack --capabilities CAPABILITY_IAM --stack-name $STAC
 	ParameterKey=TargetGroup,UsePreviousValue=true \
 	ParameterKey=TargetGroup2,UsePreviousValue=true \
 	ParameterKey=Stage2FargateStackName,ParameterValue=$TARGET_STACK_PREFIX-ecs \
-	ParameterKey=Stage2LoadBalancerStack,ParameterValue=$TARGET_STACK_PREFIX-alb
+	ParameterKey=Stage2LoadBalancerStack,ParameterValue=$TARGET_STACK_PREFIX-alb \
+	ParameterKey=Stage2DBPassSSMName,ParameterValue=$TARGET_DB_PASSWORD_PARAM_NAME \
+	ParameterKey=Stage2DatabaseStack,ParameterValue=$TARGET_STACK_PREFIX-rds \
+	ParameterKey=Stage2NetworkStack,ParameterValue=$TARGET_STACK_PREFIX-sg \
+	ParameterKey=Stage2TargetGroup,ParameterValue=$TARGET_TARGET_GROUP1 \
+	ParameterKey=Stage2TargetGroup2,ParameterValue=$TARGET_TARGET_GROUP2 \
+	ParameterKey=Stage2CloudFormationRole,ParameterValue=$TARGET_CLOUD_FORMATION_ROLE
