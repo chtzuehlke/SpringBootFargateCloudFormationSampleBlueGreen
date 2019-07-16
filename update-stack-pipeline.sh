245042ff6396a7a1ee532dff8963a336fd80f842
Christian Tschenett <cht@zuehlke.com>
2019-07-16 13:15:22 +0200

    pipeline test

--- update-stack-pipeline.sh
+++ update-stack-pipeline.sh
@@ -3,6 +3,9 @@
 PREFIX=${1:-default}
 STACK_PREFIX="helloworld-$PREFIX"
 
+TARGET_PREFIX=${2:-default}
+TARGET_STACK_PREFIX="helloworld-$TARGET_PREFIX"
+
 aws cloudformation update-stack --capabilities CAPABILITY_IAM --stack-name $STACK_PREFIX-pipe --template-body file://cloudformation/pipeline.yaml --parameters \
 	ParameterKey=CodeCommitRepositoryARN,UsePreviousValue=true \
 	ParameterKey=CodeCommitRepositoryName,UsePreviousValue=true \
@@ -15,4 +18,6 @@ aws cloudformation update-stack --capabilities CAPABILITY_IAM --stack-name $STAC
 	ParameterKey=DBPassSSMName,UsePreviousValue=true \
 	ParameterKey=CloudFormationRole,UsePreviousValue=true \
 	ParameterKey=TargetGroup,UsePreviousValue=true \
-	ParameterKey=TargetGroup2,UsePreviousValue=true
+	ParameterKey=TargetGroup2,UsePreviousValue=true \
+	ParameterKey=Stage2FargateStackName,ParameterValue=$TARGET_STACK_PREFIX-ecs \
+	ParameterKey=Stage2LoadBalancerStack,ParameterValue=$TARGET_STACK_PREFIX-alb
