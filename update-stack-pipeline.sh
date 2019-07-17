98696762853f722404a34b94346659face266d29
Christian Tschenett <cht@zuehlke.com>
2019-07-17 22:10:37 +0200

    manual approval

--- update-stack-pipeline.sh
+++ update-stack-pipeline.sh
@@ -3,16 +3,6 @@
 PREFIX=${1:-default}
 STACK_PREFIX="helloworld-$PREFIX"
 
-TARGET_PREFIX=${2:-default}
-TARGET_STACK_PREFIX="helloworld-$TARGET_PREFIX"
-
-TARGET_DB_PASSWORD_PARAM_NAME=$TARGET_STACK_PREFIX-db-pwd
-
-TARGET_TARGET_GROUP1=$(./get-stack-output.sh $TARGET_STACK_PREFIX-alb TargetGroup)
-TARGET_TARGET_GROUP2=$(./get-stack-output.sh $TARGET_STACK_PREFIX-alb TargetGroup2)
-
-TARGET_CLOUD_FORMATION_ROLE=$(./get-stack-output.sh $TARGET_STACK_PREFIX-sg CloudFormationRole)
-
 aws cloudformation update-stack --capabilities CAPABILITY_IAM --stack-name $STACK_PREFIX-pipe --template-body file://cloudformation/pipeline.yaml --parameters \
 	ParameterKey=CodeCommitRepositoryARN,UsePreviousValue=true \
 	ParameterKey=CodeCommitRepositoryName,UsePreviousValue=true \
@@ -26,11 +16,11 @@ aws cloudformation update-stack --capabilities CAPABILITY_IAM --stack-name $STAC
 	ParameterKey=CloudFormationRole,UsePreviousValue=true \
 	ParameterKey=TargetGroup,UsePreviousValue=true \
 	ParameterKey=TargetGroup2,UsePreviousValue=true \
-	ParameterKey=Stage2FargateStackName,ParameterValue=$TARGET_STACK_PREFIX-ecs \
-	ParameterKey=Stage2LoadBalancerStack,ParameterValue=$TARGET_STACK_PREFIX-alb \
-	ParameterKey=Stage2DBPassSSMName,ParameterValue=$TARGET_DB_PASSWORD_PARAM_NAME \
-	ParameterKey=Stage2DatabaseStack,ParameterValue=$TARGET_STACK_PREFIX-rds \
-	ParameterKey=Stage2NetworkStack,ParameterValue=$TARGET_STACK_PREFIX-sg \
-	ParameterKey=Stage2TargetGroup,ParameterValue=$TARGET_TARGET_GROUP1 \
-	ParameterKey=Stage2TargetGroup2,ParameterValue=$TARGET_TARGET_GROUP2 \
-	ParameterKey=Stage2CloudFormationRole,ParameterValue=$TARGET_CLOUD_FORMATION_ROLE
+	ParameterKey=Stage2FargateStackName,UsePreviousValue=true \
+	ParameterKey=Stage2LoadBalancerStack,UsePreviousValue=true \
+	ParameterKey=Stage2DBPassSSMName,UsePreviousValue=true \
+	ParameterKey=Stage2DatabaseStack,UsePreviousValue=true \
+	ParameterKey=Stage2NetworkStack,UsePreviousValue=true \
+	ParameterKey=Stage2TargetGroup,UsePreviousValue=true \
+	ParameterKey=Stage2TargetGroup2,UsePreviousValue=true \
+	ParameterKey=Stage2CloudFormationRole,UsePreviousValue=true
